130cbfec34742cbfcb064f09782227c090027211
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-05 23:48:02 -0500

    got setState to work

--- src/Random.js
+++ src/Random.js
@@ -3,23 +3,34 @@ import React, { Component } from "react";
 class Random extends Component {
   constructor(props) {
     super(props);
-    this.state = 0;
-    // this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      num1: null,
+      num2: null,
+      num3: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-//   handleClick() {}
+  handleClick(e) {
 
-  render() {
+    
     const random1 = Math.floor(Math.random() * 8) + 1;
     const random2 = Math.floor(Math.random() * 8) + 1;
     const random3 = Math.floor(Math.random() * 8) + 1;
 
+    this.setState({
+      num1: random1,
+      num2: random2,
+      num3: random3,
+    });
+  }
+
+  render() {
     return (
       <div>
-        <h1>
-          {" "}
-          {random1} - {random2} - {random3}
-        </h1>
+          {/* <h1>{this.state.clicked ? "clicked" : "not clicked"}</h1> */}
+        <h1>{this.state.num1}</h1>
+        <button onClick={this.handleClick}>Pull Lever</button>
       </div>
     );
   }
