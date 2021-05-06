8eadf01df3463da134aa2ec382b36e80c54e7f05
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-05 22:45:18 -0500

    created random component that will create 3 random numbers

---
+++ src/Random.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Random extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+
+    render() {
+        return(
+            <div>Rando Pg</div>
+        )
+    }
+}
+
+export default Random;
