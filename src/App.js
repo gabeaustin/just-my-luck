b82156476145e33bf6b8a553bc16f0837ca826bb
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-05 23:08:45 -0500

    created random number generated for numbers 1-7

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Random from "./Random";
 
 function App() {
   return (
     <div className="App">
       <h1>Just My Luck</h1>
+
+      <Random />
     </div>
   );
 }
--- src/Random.js
+++ src/Random.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 
 class Random extends Component {
-    constructor(props) {
-        super(props);
-        
-    }
+  constructor(props) {
+    super(props);
+    this.state = 0;
+    // this.handleClick = this.handleClick.bind(this);
+  }
 
-    render() {
-        return(
-            <div>Rando Pg</div>
-        )
-    }
+//   handleClick() {}
+
+  render() {
+    const random1 = Math.floor(Math.random() * 8) + 1;
+    const random2 = Math.floor(Math.random() * 8) + 1;
+    const random3 = Math.floor(Math.random() * 8) + 1;
+
+    return (
+      <div>
+        <h1>
+          {" "}
+          {random1} - {random2} - {random3}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default Random;
