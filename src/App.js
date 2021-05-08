50cbdc151a0f6f0f0bceb485f4664f62a4adc2c0
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-07 19:21:06 -0500

    added ternary operator to remove the spin button when the user wins

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Random from "./Random";
+import Luck from "./Luck";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Just My Luck</h1>
-
-      <Random />
+    <div className="App container">
+      <Luck />
     </div>
   );
 }
