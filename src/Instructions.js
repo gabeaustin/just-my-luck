9bf230902af404ec2de8dea8e374644d205db12c
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-10 19:24:09 -0500

    updated styling on instructions page

--- src/Instructions.js
+++ src/Instructions.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 
 class Instructions extends Component {
-    render() {
-        return (
-            <div>
-                Instructions for the Game
-
-                The goal is to get three 2's in a row.
-                <ul>
-                    <li>Click the Spin button multiple times until you see three 2's</li>
-                    <li>If you would like to play again, click the "Play Again" button</li>
-                    <li>Have Fun!!</li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="display-3 instructions">Instructions for the Game</div>
+        <div className="instructions">
+          <h3>The goal is to get three 2's in a row.</h3>
+        </div>
+        <div>
+          <ul>
+            <li>Click the Spin button multiple times until you see three 2's.</li>
+            <li>If you would like to play again, click the "Play Again" button.</li>
+            <li>Have Fun!!</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Instructions;
